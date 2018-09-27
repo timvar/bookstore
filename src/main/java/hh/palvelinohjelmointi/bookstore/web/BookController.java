d0be06f030936b8f49abb39f3a8221f390a2db51
Varila Timo <a1800561@myy.haaga-helia.fi>
2018-09-27 10:46:55 +0300

    booklist REST, book/id REST

--- src/main/java/hh/palvelinohjelmointi/bookstore/web/BookController.java
+++ src/main/java/hh/palvelinohjelmointi/bookstore/web/BookController.java
@@ -1,11 +1,18 @@
 package hh.palvelinohjelmointi.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import hh.palvelinohjelmointi.bookstore.domain.Book;
 import hh.palvelinohjelmointi.bookstore.repository.BookRepository;
@@ -33,6 +40,19 @@ public class BookController {
 		
 		return "booklist";
 	}
+	
+	// show all books REST
+	@GetMapping(value="/books")
+	public @ResponseBody List<Book> showBookListRest(Model model) {
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	// get book by id REST
+    @GetMapping(value="/books/{id}")
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return bookRepository.findById(bookId);
+    }     
+	
 	
 	// add book
 	@GetMapping(value = "/add")
